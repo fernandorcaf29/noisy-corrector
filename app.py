e3e938b965644acb39decdcc2364e0ee3e283620
RafaelRozaa <rafaelabreu2003@gmail.com>
2025-09-18 15:48:41 -0300

    Update app.py with log filter for progress status

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from config import UPLOAD_FOLDER, MAX_CONTENT_LENGTH
 from routes.main import bp as main_bp
+import logging
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = MAX_CONTENT_LENGTH
+
+class ProgressFilter(logging.Filter):
+    def filter(self, record):
+        return '/progress_status' not in record.getMessage()
+
+# Aplicar o filter
+log = logging.getLogger('werkzeug')
+log.addFilter(ProgressFilter())
 
 app.register_blueprint(main_bp)
 
